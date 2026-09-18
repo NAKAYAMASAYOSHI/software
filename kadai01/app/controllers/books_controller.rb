8fa9e7f3175d53b46474dcd4610534b23762818e
ナカヤマサヨシ <2411140064d@ed.fuk.kindai.ac.jp>
2026-09-18 05:05:06 +0000

    発展課題01

--- kadai01/app/controllers/books_controller.rb
+++ kadai01/app/controllers/books_controller.rb
@@ -17,13 +17,17 @@ class BooksController < ApplicationController
 
   # POST /books
   def create
-    book = Book.new(
+    @book = Book.new(
       title: params[:book][:title],
       author: params[:book][:author],
       published_year: params[:book][:published_year]
     )
-    book.save
-    redirect_to '/books'
+    if @book.save
+      redirect_to '/books', notice: "書籍を登録しました。"
+    else
+      flash.now[:alert] = "入力内容にエラーがあります。"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # GET /books/:id/edit
@@ -33,19 +37,23 @@ class BooksController < ApplicationController
 
   # PATCH/PUT /books/:id
   def update
-    book = Book.find(params[:id])
-    book.update(
+    @book = Book.find(params[:id])
+    if @book.update(
       title: params[:book][:title],
       author: params[:book][:author],
       published_year: params[:book][:published_year]
     )
-    redirect_to '/books'
+      redirect_to '/books', notice: "書籍を更新しました。"
+    else
+      flash.now[:alert] = "入力内容にエラーがあります。"
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   # DELETE /books/:id
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to '/books', notice: "書籍を削除しました。"
   end
 end
--- kadai01/app/models/book.rb
+++ kadai01/app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  validates :title, presence: { message: "を入力してください" }
+  validates :author, presence: { message: "を入力してください" }
+  validates :published_year, presence: { message: "を入力してください" },
+                              numericality: { only_integer: true, message: "は数値で入力してください" }
 end
